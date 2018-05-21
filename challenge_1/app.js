6d3ce4ecb900a7b4851f6f9fa58c4a0b8470f7d9
Eric Nague <ennague@gmail.com>
2018-05-21 15:48:54 -0700

    board is setup and toggle works

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,46 @@
 
+var game = {
+	player: 'Player X'
+	currentPlayer: true,
+	moves: 0,
 
+}
+
+//Functions
+
+
+//Reload a new game
+var newGame = function() {
+	window.location.reload()
+}
+
+
+
+var move= function(row,column) {
+	console.log(row,column);
+
+	if(game.currentPlayer) {
+		document.getElementById("cell<%='"row + column=%>"").innerHTML= "X"
+		game.currentPlayer=false;
+		game.moves++;
+	} else {
+		document.getElementById("cell<%="row + column=%>"").innerHTML= 'O'
+		game.currentPlayer= true;
+		game.moves++;
+	}
+
+	if(game.moves === 9) {
+		document.getElementById('results').innerHTML = 'We Have a Tie'
+	}
+}
+
+
+//helper functions
+
+//check if down win
+
+//check to see if across win
+
+//check to see if major diagonal win
+
+//check to see if minor diagonal win
