82410c99b594b159c562e80050165a5fa0f72f6f
Eric Nague <ennague@gmail.com>
2018-05-22 13:13:17 -0700

    jquery installed, button can get value and clear after input

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,9 @@
+$(document).ready(function() {
 
-
+	$("button#submit").on('click', function(event) {
+		event.preventDefault();
+		console.log($("input:first").val());
+		$("input:first").val('')
+	})
+	
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var app = express();
 
+
+
+
+app.use(express.static('client'));
+
+
+app.listen(3000, ()=> console.log('listening to port 3000'));
 
