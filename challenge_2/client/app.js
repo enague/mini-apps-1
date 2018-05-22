a14f9718e95b1299812e143a61776d272719ad92
Eric Nague <ennague@gmail.com>
2018-05-22 15:09:48 -0700

    can send over the data object to the server as req.body

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -2,8 +2,27 @@ $(document).ready(function() {
 
 	$("button#submit").on('click', function(event) {
 		event.preventDefault();
-		console.log($("input:first").val());
-		$("input:first").val('')
+		// var data = $("input:first").val();
+		var data = $("textarea#text").val();
+
+		console.log(data)
+
+
+		//ajax POST request
+		$.ajax({
+			type: "POST",
+			url: "/csv",
+			data: data,
+			//dataType: "json",
+			contentType: "application/json",
+			success: function(data) {
+				console.log('enters success')
+				//$("#output").append("<p>test</p>")
+			},
+			error: function(err) {
+				console.log('Error occurred', err)
+			}
+		});
 	})
 	
 })
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,13 +1,20 @@
 var http = require('http');
 var express = require('express');
-var path = require('path');
+var bodyParser = require('body-parser')
+
 var app = express();
-
-
-
+app.use(bodyParser.json());
 
 app.use(express.static('client'));
 
+app.post('/csv', function(req, res) {
+	console.log("Request url is " + req.url + " and method is " + req.method)
+	console.log(req.body)
+
+	//convert req.body to csv
+	//res.end with csv
+	res.end(console.log('end of response'));
+})
 
 app.listen(3000, ()=> console.log('listening to port 3000'));
 
