a162a38eab36714b48c9d4ae5c0ff1912b64e3ca
Eric Nague <ennague@gmail.com>
2018-05-23 12:33:23 -0700

    forms have text fieldswith breaks in between

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -13,8 +13,7 @@ class App extends React.Component {
 
 		return (
 			<div>
-				<p>Please fill out your name, email, and password. Then, hit the <b>Next</b> button</p>
-				<F1/>
+				<F1 />
 				<F2 />
 				<F3 />
 			</div>
@@ -25,21 +24,53 @@ class App extends React.Component {
 
 var F1 = () => (
 	<div>
-	<h1>hi from F1</h1>
-	<button>Next</button>
+		<p>Please fill out your name, email, and password. Then, hit the <b>Next</b> button</p>
+		<form>
+			Name:<br></br>
+			<input type="text" name="name"></input><br></br>
+			Email:<br></br>
+			<input type="text" name="email"></input><br></br>
+			Password:<br></br>
+			<input type="text" name="password"></input><br></br>
+		</form>
+		<button>Next</button>
 	</div>
 );
 
 var F2 =() => (
 	<div>
-	<h1> hi from F2</h1>
-	<button>Next</button>
+		<p>Please fill out your address and phone number. Then, hit the <b>Next</b> button</p>
+		<form>
+			Line 1:<br></br>
+			<input type="text" name="line1"></input><br></br>
+			Line 2:<br></br>
+			<input type="text" name="line2"></input><br></br>
+			City:<br></br>
+			<input type="text" name="city"></input><br></br>
+			State:<br></br>
+			<input type="text" name="state"></input><br></br>
+			Zip Code:<br></br>
+			<input type="text" name="zip"></input><br></br>
+			Phone Number:<br></br>
+			<input type="text" name="phone"></input><br></br>
+		</form>
+		<button>Next</button>
 	</div>
 );
 
 var F3 = () => (
 	<div>
-	<h1> hi from F3 </h1>
+	<p>Please fill out your credit card #, expiry date, CVV, and billing zip code. Then, hit the <b>Purchase</b> button</p>
+	<form>
+		Credit Card #:<br></br>
+		<input type="text" name="cc#"></input><br></br>
+		Expiry Date:<br></br>
+		<input type="text" name="expiry"></input><br></br>
+		CVV:<br></br>
+		<input type="text" name="cvv"></input><br></br>
+		Billing Zip Code:<br></br>
+		<input type="text" name="billingZip"></input><br></br>
+	</form>
 	<button>Purchase</button>
 	</div>
 );
